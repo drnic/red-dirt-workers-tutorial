be95313000d4491fb4aca2a961d345029e32dc7e
Dr Nic Williams <drnicwilliams@gmail.com>
2011-04-22 10:19:49 -0500

    Use setInterval & clearInterval to poll for new data regularly (and stop it)

--- public/application.js
+++ public/application.js
@@ -3,10 +3,18 @@ function fetch(fragment_name) {
     url: "/fragments/" + fragment_name,
     data: $('#search').serialize(),
     success: function(data, status, req) {
-      $('#' + fragment_name).html(data);
+      $('#' + fragment_name).append(data);
     },
     error: function(request, status) {
       console.log(status);
     }
   });
 }
+
+$(document).ready(function() {
+  $('#stop-polling').click(function() {
+    if (typeof document.pollId != "undefined" && document.pollId !== null) {
+      clearInterval(document.pollId);
+    }
+  });
+});
